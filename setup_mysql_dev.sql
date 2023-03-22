0507359e1377499f524c673101568a6ed174afc6
loyd ngei <loydngei@gmail.com>
2023-03-22 09:18:13 +0300

    Set up mysql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- create database and user
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER If NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
