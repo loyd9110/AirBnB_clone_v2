8628eb8fe083535691a99c92d3efd714c1abcb46
loyd ngei <loydngei@gmail.com>
2023-04-06 13:51:22 -0700

    Do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-""" A fabric script that distributes an archive to servers """
+"""Deploying the webstatic to the server
+"""
+from fabric.api import env, put, run
+import os
 
-from fabric.api import run, env, put
-import os.path
+env.hosts = ["54.237.227.137", "54.237.62.143"]
 
-env.hosts = ['54.237.227.137','54.237.62.143']
-env.key_filename = '~/.ssh/school'
-env.user = 'ubuntu'
 
 def do_deploy(archive_path):
-""" The deploy function starts """ 
+
     if not os.path.isfile(archive_path):
         return False
+
     to_compress = archive_path.split("/")[-1]
     extension_none = to_compress.split(".")[0]
 
+    The_path = "/data/web_static/releases/{}".format(extension_none)
     try:
-        the_path = "/data/web_static/releases/{}/".format(extension_none)
-        symbolic_link = "/data/web_static/current"
         put(archive_path, "/tmp/")
-        run("sudo tar -xvzf /tmp/{} -C {}".format(to_compress, the_path))
+        run("sudo mkdir -p {}".format(The_path))
+        run("sudo tar -xvzf /tmp/{} -C {}".format(to_compress, The_path))
         run("sudo rm /tmp/{}".format(to_compress))
-        run("sudo ln -sf {} {}".format(the_path, symbolic_link))
-       return True
+        run("sudo ln -sf {} /data/web_static/current".format(The_path))
     except Exception as e:
-       return False
+        return False
+    return True
