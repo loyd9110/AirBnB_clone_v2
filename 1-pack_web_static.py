3d39af891817c2207a2eec76e20c5aa70f2723f0
loyd ngei <loydngei@gmail.com>
2023-04-06 13:05:51 -0700

    Compress

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Fabric script to create an archive file """
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
 
 
@@ -8,11 +8,11 @@ def do_pack():
     """ Compression of a file and return path """
     """ Current timestamp and file path """
     time_created = datetime.now().strftime("%Y%m%d%H%M%S")
-    file_path = f"versions/web_static_{time_created}.tgz"
+    file_path = "versions/web_static_{}.tgz".format(time_created)
 
     try:
         local("mkdir -p versions")
-        local(f"tar -cvzf {file_path} web_static/")
-        return f"{file_path}"
+        local("tar -cvzf {} web_static".format(file_path))
+        return file_path
     except Exception as e:
         return None
--- 2-do_deploy_web_static.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" A fabric script that distributes an archive to servers """
-
-from fabric.api import run, env, put
-import os.path
-from fabric import Connection
-
-def do_deploy(archive_path):
-
