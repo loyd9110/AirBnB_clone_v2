51a81ed97babc57c33d00103ea59bce3ad5b3e17
loyd ngei <loydngei@gmail.com>
2023-04-07 04:27:02 -0700

    Deploy web_static

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""a fabric script to create an archive file
+and deployes it to a remote server """
+from fabric.api import local
+from datetime import datetime
+from fabric.api import run, env, put
+import os.path
+
+env.hosts = ['54.237.227.137', '54.237.62.143']
+env.key_filename = '~/.ssh/school'
+env.user = 'ubuntu'
+
+def do_pack():
+    """ a method to compress a file and return it's path """
+
+    """saving the current timestamp and creatinf filename"""
+    time_created = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_path = "versions/web_static_{}.tgz".format(time_created)
+
+    try:
+        """create a directory called versions"""
+        local("mkdir -p versions")
+
+        """create an archive file"""
+        local("tar -cvzf {} web_static/".format(file_path))
+
+        """return the path to the archive file created"""
+        return file_path
+
+        """return none if an error occurs"""
+    except Exception as e:
+        return None
+
+def do_deploy(archive_path):
+    """a function to deploy code and decompress it"""
+
+    if not os.path.isfile(archive_path):
+        return False
+    compressed_file = archive_path.split("/")[-1]
+    no_extension = compressed_file.split(".")[0]
+
+    try:
+       remote_path = "/data/web_static/releases/{}/".format(no_extension)
+       sym_link = "/data/web_static/current"
+       put(archive_path, "/tmp/")
+       run("sudo mkdir -p {}".format(remote_path))
+       run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
+       run("sudo rm /tmp/{}".format(compressed_file))
+       run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
+       run("sudo rm -rf {}/web_static".format(remote_path))
+       run("sudo rm -rf /data/web_static/current")
+       run("sudo ln -sf {} {}".format(remote_path, sym_link))
+       return True
+    except Exception as e:
+       return False
+
+def deploy():
+    """Create and deploy an archive to a web server."""
+    file_path = do_pack()
+    if file_path is None:
+        return False
+    return do_deploy(file_path)
