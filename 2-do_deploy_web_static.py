da164cb86ec197783e92bc84493201f20ffd5ef8
loyd ngei <loydngei@gmail.com>
2023-04-07 02:04:57 -0700

    Deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""Deploying the webstatic to the server
-"""
-from fabric.api import env, put, run
-import os
+"""a script to send an archive file to a remote server
+and decompress it"""
 
-env.hosts = ["54.237.227.137", "54.237.62.143"]
+from fabric.api import run, env, put
+import os.path
+
+env.hosts = ['54.237.227.137', '54.237.62.143']
+env.key_filename = '~/.ssh/school'
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-
+    """a function to deploy code and decompress it"""
     if not os.path.isfile(archive_path):
         return False
-
-    to_compress = archive_path.split("/")[-1]
-    extension_none = to_compress.split(".")[0]
-
-    The_path = "/data/web_static/releases/{}".format(extension_none)
+    compressed_file = archive_path.split("/")[-1]
+    no_extension = compressed_file.split(".")[0]
     try:
+        remote_path = "/data/web_static/releases/{}/".format(no_extension)
+        sym_link = "/data/web_static/current"
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(The_path))
-        run("sudo tar -xvzf /tmp/{} -C {}".format(to_compress, The_path))
-        run("sudo rm /tmp/{}".format(to_compress))
-        run("sudo ln -sf {} /data/web_static/current".format(The_path))
+        run("sudo mkdir -p {}".format(remote_path))
+        run("sudo tar -xvzf /tmp/{} \
+            -C {}".format(compressed_file, remote_path))
+        run("sudo rm /tmp/{}".format(compressed_file))
+        run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
+        run("sudo rm -rf {}/web_static".format(remote_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf {} {}".format(remote_path, sym_link))
+        return True
     except Exception as e:
         return False
-    return True
