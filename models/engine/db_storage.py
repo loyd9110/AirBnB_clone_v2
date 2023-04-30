4014c2ba92f243d6b9e459b2680513829cc57539
loyd ngei <loydngei@gmail.com>
2023-04-30 15:44:42 +0300

    List of states

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,13 +19,13 @@ class DBStorage():
 
     def __init__(self):
         """ initiliazes the class """
-        DBStorage.__engine = create_engine(
+        self.__engine = create_engine(
              f'mysql+mysqldb://{user}:{password}@{host}/{database}',
              pool_pre_ping=True
         )
         hbnd_env = os.environ.get('HBNB_ENV')
         if (hbnd_env == "test"):
-            Base.metadata.drop_all(DBStorage.__engine)
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         """ retrives all objects """
@@ -36,38 +36,38 @@ class DBStorage():
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-        Session = sessionmaker(bind=DBStorage.__engine)
-        DBStorage.__session = Session()
+        Session = sessionmaker(bind=self.__engine)
+        self.__session = Session()
         objects = {}
         if cls is not None:
             # Query for objects of a specific class
-            results = DBStorage.__session.query(cls).all()
+            results = self.__session.query(cls).all()
         else:
             # Query for all types of objects
             results = []
             for cls in [State, City, User, Place, Review, Amenity]:
-                results.extend(DBStorage.__session.query(cls).all())
+                results.extend(self.__session.query(cls).all())
         # Add objects to dictionary
         for obj in results:
             key = "{}.{}".format(obj.__class__.__name__, obj.id)
             objects[key] = obj
-        DBStorage.__session.close()
+        self.__session.close()
         return objects
 
     def new(self, obj):
         """ add a new object to the session """
 
-        DBStorage.__session.add(obj)
+        self.__session.add(obj)
 
     def save(self):
         """ saves an object to the database """
 
-        DBStorage.__session.commit()
+        self.__session.commit()
 
     def delete(self, obj=None):
         """ deletes an object from current session """
         if (obj):
-            DBStorage.__session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
         from models.base_model import BaseModel
@@ -79,15 +79,15 @@ class DBStorage():
         from models.review import Review
 
         # create all tables in the database
-        Base.metadata.create_all(DBStorage.__engine)
+        Base.metadata.create_all(self.__engine)
 
         # create the current database session
         session_factory = sessionmaker(
-            bind=DBStorage.__engine,
+            bind=self.__engine,
             expire_on_commit=False
         )
-        DBStorage.__session = scoped_session(session_factory)
+        self.__session = scoped_session(session_factory)
 
     def close(self):
         """Call remove method"""
-        DBStorage.__session.close()
+        self.__session.close()
---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Module contains the app instance for the AirBnB Web"""
+
+from models import storage
+from models.state import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/state_list', strict_slashes=False)
+def states_list():
+    """enumerate all states"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def clean_up(exception=None):
+    """Eliminates current session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
